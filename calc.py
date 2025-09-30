3556e9f171f777d3f0830ede91378b26986a2726
Navaneeth <navaneethk969@gmail.com>
2025-09-30 10:34:59 +0530

    Adds program to find squreroot

--- calc.py
+++ calc.py
@@ -15,4 +15,14 @@ num2 = int(input("Enter second number :"))
 
 # Squreroot
 
+if num1 >= 0:
+    print("Square root of first number:", math.sqrt(num1))
+else:
+    print("Square root of first number: Not defined for negative numbers")
+
+if num2 >= 0:
+    print("Square root of second number:", math.sqrt(num2))
+else:
+    print("Square root of second number: Not defined for negative numbers")
+
 #  factorial
