e47939352ffb95722e422c4f8b30ed2408ec9f5b
Navaneeth <navaneethk969@gmail.com>
2025-09-30 10:14:31 +0530

    Initial commit

---
+++ calc.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+
+# Addition
+
+# Subtraction
+
+# Division
+
+# Multiplication
+
+# exponential
+
+# Square
+
+# Squreroot
+
+#  factorial
