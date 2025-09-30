04f0c5dedfad72c31c1ccd2e272ae68dc1f64e6b
NavaneethaPrasad <navaneethaprasad25092002@gmail.com>
2025-09-30 10:51:48 +0530

    Division

--- calc.py
+++ calc.py
@@ -6,6 +6,12 @@ num2 = int(input("Enter second number :"))
 # Subtraction
 
 # Division
+def division(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+    
+print("Division:", division(num1, num2))
 
 # Multiplication
 
