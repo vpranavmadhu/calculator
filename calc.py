3ec13401f209d6b7113cec91fa74733530886eb9
ayman <aymanrafeeque419@gmail.com>
2025-09-30 10:34:09 +0530

    added factorial

--- calc.py
+++ calc.py
@@ -16,3 +16,12 @@ num2 = int(input("Enter second number :"))
 # Squreroot
 
 #  factorial
+if num1 >= 0:
+    print("Factorial of num1:", math.factorial(num1))
+else:
+    print("Factorial of num1: Not defined for negative numbers.")
+
+if num2 >= 0:
+    print("Factorial of num2:", math.factorial(num2))
+else:
+    print("Factorial of num2: Not defined for negative numbers.")
